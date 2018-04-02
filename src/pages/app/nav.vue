9995d44391da8f7d13a697bdc0625cf22cb88552
sujintao <sujintao@bullstech1deMac-mini.local>
2018-04-02 23:14:22 +0800

    taotao

--- src/pages/app/nav.vue
+++ src/pages/app/nav.vue
@@ -1,16 +1,24 @@
 <template>
     <yd-tabbar active-color="#ed293f">
         <yd-tabbar-item title="首页" link="/" :active="istrend">
-            <yd-icon name="home" slot="icon" size="0.54rem" active></yd-icon>
+            <!-- <yd-icon name="home" slot="icon" size="0.54rem" active></yd-icon> -->
+            <img v-if="!istrend" slot="icon" src="/assets/images/trend.png" size="0.54rem">
+            <img v-if="istrend" slot="icon" src="/assets/images/trend-active.png" size="0.54rem">
         </yd-tabbar-item>
         <yd-tabbar-item title="购物车" link="/detailview" :active="isdetailview">
-            <yd-icon name="shopcart-outline" slot="icon" size="0.54rem"></yd-icon>
+            <!-- <yd-icon name="shopcart-outline" slot="icon" size="0.54rem"></yd-icon> -->
+            <img v-if="!isdetailview" slot="icon" src="/assets/images/detail.png" size="0.54rem">
+            <img v-if="isdetailview" slot="icon" src="/assets/images/detail-active.png" size="0.54rem">
         </yd-tabbar-item>
         <yd-tabbar-item title="个人中心" link="/explain" :active="isexplain">
-            <yd-icon name="ucenter-outline" slot="icon" size="0.54rem"></yd-icon>
+            <!-- <yd-icon name="ucenter-outline" slot="icon" size="0.54rem"></yd-icon> -->
+            <img v-if="!isexplain" slot="icon" src="/assets/images/explain.png" size="0.54rem">
+            <img v-if="isexplain" slot="icon" src="/assets/images/explain-active.png" size="0.54rem">
         </yd-tabbar-item>
         <yd-tabbar-item title="图片" link="/person" :active="isperson">
-            <img slot="icon" src="http://static.ydcss.com/ydui/img/logo.png">
+            <!-- <img slot="icon" src="http://static.ydcss.com/ydui/img/logo.png"> -->
+            <img v-if="!isperson" slot="icon" src="/assets/images/person.png" size="0.54rem">
+            <img v-if="isperson" slot="icon" src="/assets/images/person-active.png" size="0.54rem">
         </yd-tabbar-item>
     </yd-tabbar>
 </template>
@@ -29,9 +37,8 @@
             "$route":"routelinkchange"
         },
         computed: {
-
+            
         },
-
         methods: {
             routelinkchange(){
                 let path = this.$route.path
@@ -65,3 +72,8 @@
         }
     }
 </script>
+<style>
+.yd-tabbar-icon img{
+    height: 83% !important;
+}
+</style>
