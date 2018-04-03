57b01a65c026dd8d53a651c9b0baba4f73892f3d
sujintao <sujintao@bullstech1deMac-mini.local>
2018-04-03 11:15:03 +0800

    taotao

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import './style/index.css'
 import './style/module.css'
 import App from './pages/app/app.vue'
-// import lbCom from './components'
 import router from './router'
-import mixin from './mixins/mixin'
+// import lbCom from './components'
+// import mixin from './mixins/mixin'
 import 'vue-ydui/dist/ydui.base.css'
 import YDUI from 'vue-ydui'
 
-Vue.use(Vuex)
-Vue.mixin(mixin)
+
+// Vue.use(Vuex)
+// Vue.mixin(mixin)
 // Vue.use(lbCom)
 Vue.use(YDUI)
 
--- src/pages/view/person.vue
+++ src/pages/view/person.vue
@@ -10,15 +10,34 @@
             <span slot="right">个人信息</span>
         </yd-cell-item>
       </yd-cell-group>
-      
+      <yd-cell-group class="personlist">
+        <yd-cell-item arrow type="a" href="tel:400-888-8888">
+            <span slot="left">这里是一个A链接</span>
+        </yd-cell-item>
+        <yd-cell-item arrow type="label">
+            <span slot="left">这里是一个Router-Link链接</span>
+        </yd-cell-item>
+        <yd-cell-item arrow type="label">
+            <span slot="left">这里是一个Label</span>
+        </yd-cell-item>
+        <yd-cell-item arrow>
+            <span slot="left">这里是一个普通DIV</span>
+        </yd-cell-item>
+      </yd-cell-group>
+      <yd-button-group>
+            <yd-button size="large" bgcolor="#ed293f" color="#fff">退出登录</yd-button>
+      </yd-button-group>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
 import {CellGroup, CellItem} from 'vue-ydui/dist/lib.rem/cell'
-Vue.component(CellGroup.name, CellGroup);
-Vue.component(CellItem.name, CellItem);
+import {Button, ButtonGroup} from 'vue-ydui/dist/lib.rem/button'
+Vue.component(CellGroup.name, CellGroup)
+Vue.component(CellItem.name, CellItem)
+Vue.component(Button.name, Button)
+Vue.component(ButtonGroup.name, ButtonGroup)
 export default {
     name: 'person',
     data() {
@@ -45,6 +64,9 @@ export default {
 .uerimages{
     height: 1.5rem !important;
     margin: 0.3rem 0;
+}
+.personlist{
+    margin-top: 0.5rem !important;
 }
 </style>
 
